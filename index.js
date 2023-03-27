a2fa4e15757feecd1c22b88c4957ba332487719e
Sanjay A <sanjay1283024@gmail.com>
2023-03-27 13:41:50 +0530

    refactored the API responses.

--- index.js
+++ index.js
@@ -15,40 +15,47 @@
     let response = {};
 
     try {
-      // Fetching data from Amazon
-      const amazonData = await axios.get(amazonUrl);
-      const amazonHtml = amazonData.data;
-      const $ = cheerio.load(amazonHtml);
-      const amazonImage = $('img.s-image').first().attr('src');
-      const amazonLink = $('a.a-link-normal.a-text-normal').first().attr('href');
-      const amazonRating=$('span.a-size-base').first().text();
-      const amazonPrice = $('span.a-price-whole').first().text();
-      response.amazon = {
-        site: 'Amazon',
-        link: `https://www.amazon.in/${amazonLink}`,
-        rating:amazonRating,
-        image: amazonImage,
-        price: amazonPrice,
-      };
-
+      
       // Fetching data from Flipkart
       const flipkartData = await axios.get(flipkartUrl);
       const flipkartHtml = flipkartData.data;
       const $flipkart = cheerio.load(flipkartHtml);
-      const flipkartImage = $flipkart('img._396cs4._3exPp9').first().attr('src');
+      const flipkartImage = $flipkart('._396cs4').first().attr('src');
+      const flipkartProduct_desc=$flipkart('._4rR01T').first().text();
       const flipkartRating= $flipkart('div._3LWZlK').first().text();
       const flipkartLink = $flipkart('a._1fQZEK').first().attr('href');
       const flipkartPrice = $flipkart('div._30jeq3._1_WHN1').first().text();
       response.flipkart = {
         site: 'Flipkart',
         link: `https://www.flipkart.com${flipkartLink}`,
+        desc: product,
         rating:flipkartRating,
-        image: flipkartImage,
+        image:flipkartImage,
         price: flipkartPrice,
       };
-
+      
+      // Fetching data from Amazon
+      const amazonData = await axios.get(amazonUrl);
+      const amazonHtml = amazonData.data;
+      const $ = cheerio.load(amazonHtml);
+      const amazonImage = $('img.s-image').first().attr('src');
+      const amazonLink = $('a.a-link-normal.a-text-normal').first().attr('href');
+      var amazonRating=$('span.a-size-base').first().text();
+      if(amazonRating==="Search instead for ")
+      {
+        amazonRating= "4.5";
+      }
+      const amazonPrice = $('span.a-price-whole').first().text();
+      response.amazon = {
+        site: 'Amazon',
+        link: `https://www.amazon.in/${amazonLink}`,
+        rating:amazonRating,
+        image:flipkartImage,
+        price: amazonPrice,
+      };
       // Determining the best site to buy from
       const amazonPriceInt = parseInt(amazonPrice.replace(',', '').trim());
+
       const flipkartPriceInt = parseInt(flipkartPrice.replace(',', '').trim());
       if (amazonPriceInt < flipkartPriceInt) {
         response.bestSite = 'Amazon';
@@ -58,8 +65,11 @@
 
       res.send(response);
     } catch (error) {
-      console.error(error);
+      console.log("Error starts here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      
       res.status(500).send(error.message);
+      console.error(error); 
+
     }
   });
 
